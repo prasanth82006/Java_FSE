48e42c24e94748c09d0af3455e242b0aea5ba4ee
prasanth82006 <prasanthkumar905241@gmail.com>
2026-06-29 18:23:18 +0530

    Your commit message

---
+++ Module_5/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,21 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter method for dependency injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook() {
+        System.out.println("BookService: Adding book...");
+        if (bookRepository != null) {
+            bookRepository.save();
+        } else {
+            System.out.println("BookService: Error - BookRepository is not injected!");
+        }
+    }
+}
